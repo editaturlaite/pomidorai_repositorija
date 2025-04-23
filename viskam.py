dd921126da14853b7c7f758b39febb5750793a4c
Edita <editaturlaite@ymail.com>
2025-04-23 21:30:29 +0300

    sukurta saka duomenu apdorojimui

--- viskam.py
+++ viskam.py
@@ -5,3 +5,8 @@ train_df = image_dataset_from_directory(r"C:\Users\Vartotojas\Desktop\POMIDORAI\
 
 validation_df = image_dataset_from_directory(r"C:\Users\Vartotojas\Desktop\POMIDORAI\archive (1)\plantvillage",validation_split = 0.2,
                                         subset="validation",seed=42,image_size = (224,224), batch_size=32 )
+
+
+# ---------------------------------------------------------
+
+
