f60ca61be23072cbf18bfeef249aa94f3b5c970a
Edita <editaturlaite@ymail.com>
2025-04-30 21:46:48 +0300

    pakeisti duomenu keliai nes sukurti aplankai  rankiniu budu. sukurta ir paruosta train val test duomenu rinkiniai

--- viskam.py
+++ viskam.py
@@ -1,12 +1,31 @@
-from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-train_df = image_dataset_from_directory(r"C:\Users\Vartotojas\Desktop\POMIDORAI\archive (1)\plantvillage",validation_split = 0.2,
-                                        subset="training",seed=42,image_size = (224,224), batch_size=32)
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
-validation_df = image_dataset_from_directory(r"C:\Users\Vartotojas\Desktop\POMIDORAI\archive (1)\plantvillage",validation_split = 0.2,
-                                        subset="validation",seed=42,image_size = (224,224), batch_size=32 )
 
+train_kelias = r"C:\Users\Vartotojas\Desktop\POMIDORAI\pomidoru_duomenys\trenyravimas"
+val_kelias = r"C:\Users\Vartotojas\Desktop\POMIDORAI\pomidoru_duomenys\validacija"
+test_kelias = r"C:\Users\Vartotojas\Desktop\POMIDORAI\pomidoru_duomenys\testas"
+
+train_df = tf.keras.utils.image_dataset_from_directory(train_kelias,image_size=(224, 224),batch_size=32)
+
+val_df = tf.keras.utils.image_dataset_from_directory(val_kelias,image_size=(224, 224),batch_size=32)
+
+test_df = tf.keras.utils.image_dataset_from_directory(test_kelias,image_size=(224, 224),batch_size=32)
 
 # ---------------------------------------------------------
 
+class_names = train_df.class_names
+print(class_names)
+
+for images, labels in train_df.take(1):
+    plt.figure(figsize=(10, 10))
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+        # plt.show()
+
+# ----------------------------------------------------------------
 
