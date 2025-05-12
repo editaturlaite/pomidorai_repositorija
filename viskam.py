5e22fd8a7ce3b2ce03f536478b96d84354542aac
Edita <editaturlaite@ymail.com>
2025-05-12 12:00:06 +0300

    sukurtos duomenu bazes lenteles informacijai keliams paveiksleliu. 3 lenteles train val ir test duomenims

--- viskam.py
+++ viskam.py
@@ -17,6 +17,123 @@ test_df = tf.keras.utils.image_dataset_from_directory(test_kelias,image_size=(22
 
 # ---------------------------------------------------------
 
+from sqlalchemy import create_engine, Column, Integer, String, LargeBinary
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from sqlalchemy.orm import declarative_base
+
+Bazine_klase = declarative_base()
+rysys_su_baze = create_engine('sqlite:///pomidoru_lapai.db')
+
+Session = sessionmaker(bind=rysys_su_baze)
+sesija = Session()
+
+class PomidoraiTrenyravimas(Bazine_klase):
+
+    __tablename__ = 'Pomidoru_lapai_trenyravimo_duomenys'
+
+    id = Column(Integer,primary_key = True)
+    kelias = Column(String, unique=True, nullable=False)  
+    klases_pavadinimas = Column(String, nullable=False)
+
+
+def irasyti_trenyravimo_paveikslelius(sesija, trenyravimo_kelias):
+
+    for klases_pavadinimas in os.listdir(trenyravimo_kelias):
+        klases_kelias = os.path.join(trenyravimo_kelias,klases_pavadinimas)
+
+        if os.path.isdir(klases_kelias):
+            for paveikslelio_pavadinimas in os.listdir(klases_kelias):
+                paveikslelio_kelias = os.path.join(klases_kelias,paveikslelio_pavadinimas)
+
+                egzistuoja = sesija.query(PomidoraiTrenyravimas).filter_by(kelias=paveikslelio_kelias).first()
+
+                if not egzistuoja:
+
+                    naujas_irasas = PomidoraiTrenyravimas(kelias = paveikslelio_kelias, klases_pavadinimas = klases_pavadinimas)
+
+                    sesija.add(naujas_irasas)
+
+    sesija.commit()
+
+    print("Irasyti trenyravimo paveiksleiai")
+
+# --------------------------------------------------------------------------------------------------------------------------
+
+
+class PomidoraiValidacija(Bazine_klase):
+
+    __tablename__ = 'Pomidoru_lapai_validacijos_duomenys'
+
+    id = Column(Integer,primary_key = True)
+    kelias = Column(String, unique=True, nullable=False)  
+    klases_pavadinimas = Column(String, nullable=False)
+
+def irasyti_validacijos_paveikslelius(sesija, validacijos_kelias):
+
+    for klases_pavadinimas in os.listdir(validacijos_kelias):
+        klases_kelias = os.path.join(validacijos_kelias,klases_pavadinimas)
+
+        if os.path.isdir(klases_kelias):
+            for paveikslelio_pavadinimas in os.listdir(klases_kelias):
+                paveikslelio_kelias = os.path.join(klases_kelias,paveikslelio_pavadinimas)
+
+                egzistuoja = sesija.query(PomidoraiValidacija).filter_by(kelias=paveikslelio_kelias).first()
+
+                if not egzistuoja:
+
+                    naujas_irasas = PomidoraiValidacija(kelias = paveikslelio_kelias, klases_pavadinimas = klases_pavadinimas)
+
+                    sesija.add(naujas_irasas)
+
+    sesija.commit()
+
+    print("Irasyti validacijos paveiksleiai")
+
+# -------------------------------------------------------------------------------------------------------------
+
+class PomidoraiTestas(Bazine_klase):
+
+    __tablename__ = 'Pomidoru_lapai_testo_duomenys'
+
+    id = Column(Integer,primary_key = True)
+    kelias = Column(String, unique=True, nullable=False)  
+    klases_pavadinimas = Column(String, nullable=False)
+
+def irasyti_testo_paveikslelius(sesija, testo_kelias):
+
+    for klases_pavadinimas in os.listdir(test_kelias):
+        klases_kelias = os.path.join(test_kelias,klases_pavadinimas)
+
+        if os.path.isdir(klases_kelias):
+            for paveikslelio_pavadinimas in os.listdir(klases_kelias):
+                paveikslelio_kelias = os.path.join(klases_kelias,paveikslelio_pavadinimas)
+
+                egzistuoja = sesija.query(PomidoraiTestas).filter_by(kelias=paveikslelio_kelias).first()
+
+                if not egzistuoja:
+
+                    naujas_irasas = PomidoraiTestas(kelias = paveikslelio_kelias, klases_pavadinimas = klases_pavadinimas)
+
+                    sesija.add(naujas_irasas)
+
+    sesija.commit()
+
+    print("Irasyti testo paveiksleiai")
+
+# ------------------------------------------------------------------------------------------
+Bazine_klase.metadata.create_all(rysys_su_baze)
+
+irasyti_trenyravimo_paveikslelius(sesija,train_kelias)
+irasyti_validacijos_paveikslelius(sesija,val_kelias)
+irasyti_testo_paveikslelius(sesija,test_kelias)
+
+
+
+
+
+# ----------------------------------------------------------------------
+
 # class_names = train_df.class_names
 # print(class_names)
 
@@ -66,60 +183,62 @@ test_df = tf.keras.utils.image_dataset_from_directory(test_kelias,image_size=(22
 
 # fizine duomenu augmentacija
 
-import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img, save_img
-import numpy as np
-import random
+# import os
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img, save_img
+# import numpy as np
+# import random
 
 
-def sukurti_naujus_paveikslelius(duomenu_kelias, reikiamas_foto_kiekis):
+# def sukurti_naujus_paveikslelius(duomenu_kelias, reikiamas_foto_kiekis):
 
-    failu_generavimas = ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        zoom_range=0.1,
-        horizontal_flip=True,
-        fill_mode='nearest'
-    )
+#     failu_generavimas = ImageDataGenerator(
+#         rotation_range=20,
+#         width_shift_range=0.1,
+#         height_shift_range=0.1,
+#         zoom_range=0.1,
+#         horizontal_flip=True,
+#         fill_mode='nearest'
+#     )
 
-    for klase in os.listdir(duomenu_kelias): #pereina per visas klases foldery
-        klases_kelias = os.path.join(duomenu_kelias, klase) #sujungia pagrindini kelia su konkrecia klase ir sukuria jos kelia 
+#     for klase in os.listdir(duomenu_kelias): #pereina per visas klases foldery
+#         klases_kelias = os.path.join(duomenu_kelias, klase) #sujungia pagrindini kelia su konkrecia klase ir sukuria jos kelia 
 
-        try:
-            esami_failai = [f for f in os.listdir(klases_kelias) if f.endswith('.JPG')] # imamas kiekvienas failas f klases folderi ir jei atitinka idedamas i sarasa
-            kiek_truksta = reikiamas_foto_kiekis - len(esami_failai) #apskaiciuojam kiek truksta iki 2000
+#         try:
+#             esami_failai = [f for f in os.listdir(klases_kelias) if f.endswith('.JPG')] # imamas kiekvienas failas f klases folderi ir jei atitinka idedamas i sarasa
+#             kiek_truksta = reikiamas_foto_kiekis - len(esami_failai) #apskaiciuojam kiek truksta iki 2000
 
-            if kiek_truksta <= 0:
-                print(f"{klase} turi pakankamai paveiksleliu")
-                continue
+#             if kiek_truksta <= 0:
+#                 print(f"{klase} turi pakankamai paveiksleliu")
+#                 continue
 
-            sukurta = 0
-            while sukurta < kiek_truksta:
-                originalus_paveikslelis = random.choice(esami_failai) #paimamas atsitiktinis originalus paveikslelis
-                paveikslelis_kelias = os.path.join(klases_kelias, originalus_paveikslelis) #sujungia klases kelia su paveiksleliu ir sukuriamas paveikslelio kelias
-                paveikslelis = load_img(paveikslelis_kelias) #uzkraunamas paveikslelis
-                x = img_to_array(paveikslelis) #paveikslelis paverciamas i numeri np masyva
-                x = np.expand_dims(x, axis=0) #pavercia i batch (224, 224, 3) prideda nr kiek paveiksleliu partijoje (1,224, 224, 3) nes imagegeneration veikia tik taip 
+#             sukurta = 0
+#             while sukurta < kiek_truksta:
+#                 originalus_paveikslelis = random.choice(esami_failai) #paimamas atsitiktinis originalus paveikslelis
+#                 paveikslelis_kelias = os.path.join(klases_kelias, originalus_paveikslelis) #sujungia klases kelia su paveiksleliu ir sukuriamas paveikslelio kelias
+#                 paveikslelis = load_img(paveikslelis_kelias) #uzkraunamas paveikslelis
+#                 x = img_to_array(paveikslelis) #paveikslelis paverciamas i numeri np masyva
+#                 x = np.expand_dims(x, axis=0) #pavercia i batch (224, 224, 3) prideda nr kiek paveiksleliu partijoje (1,224, 224, 3) nes imagegeneration veikia tik taip 
 
-                for batch in failu_generavimas.flow(x, batch_size=1): #pradeda generuoti naujus paveikslelius is 1 paveikslelio
-                    failo_pavadinimas = f"aug_{sukurta}_{originalus_paveikslelis}"
-                    pilnas_kelias = os.path.join(klases_kelias, failo_pavadinimas) #sukuriamas kelias iki naujo sugeneruoto paveikslelio
-                    save_img(pilnas_kelias, batch[0]) #batch[0] vienas paveikslelis kaip masyvas
-                    sukurta += 1
-                    if sukurta >= kiek_truksta:
-                        break
-                    
-            print(f"Klasei {klase} sukurta {sukurta} nauju paveiksleliu")
+#                 for batch in failu_generavimas.flow(x, batch_size=1): #pradeda generuoti naujus paveikslelius is 1 paveikslelio
+#                     failo_pavadinimas = f"aug_{sukurta}_{originalus_paveikslelis}"
+#                     pilnas_kelias = os.path.join(klases_kelias, failo_pavadinimas) #sukuriamas kelias iki naujo sugeneruoto paveikslelio
+#                     save_img(pilnas_kelias, batch[0]) #batch[0] vienas paveikslelis kaip masyvas
+#                     sukurta += 1
+#                     if sukurta >= kiek_truksta:
+#                         break
 
-        except Exception as klaida:
-            print("Klaida generuojant paveikslelius")
+#             print(f"Klasei {klase} sukurta {sukurta} nauju paveiksleliu")
+
+#         except Exception as klaida:
+#             print("Klaida generuojant paveikslelius")
 
 
-reikiamas_foto_kiekis = 2000
+# reikiamas_foto_kiekis = 2000
 
-duomenu_kelias = r"C:\Users\Vartotojas\Desktop\POMIDORAI\pomidoru_duomenys\trenyravimas"
+# duomenu_kelias = r"C:\Users\Vartotojas\Desktop\POMIDORAI\pomidoru_duomenys\trenyravimas"
 
-sukurti_naujus_paveikslelius(duomenu_kelias,reikiamas_foto_kiekis)
+# sukurti_naujus_paveikslelius(duomenu_kelias,reikiamas_foto_kiekis)
+
+# ---------------------------------------------------------------------------
 
 
