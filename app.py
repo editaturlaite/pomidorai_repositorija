3d47ca8f91ab546fbce2ee53684c3600fed2e5d7
Edita <editaturlaite@ymail.com>
2025-05-19 14:23:15 +0300

    sutvarkyta struktura vartotojo prognozems isvalytas app failas

--- app.py
+++ app.py
@@ -12,92 +12,7 @@ from scipy.special import softmax
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
-
-
-def prognozuoti_su_cnn(nuotraukos_kelias, klases, dydis=(128, 128)):
-    modelis = load_model("issaugoti_modeliai/paprastas_cnn_962.h5")
-
-    atidarytas_paveikslelis = load_img(nuotraukos_kelias, target_size=dydis)
-    paveikslelis_array = img_to_array(atidarytas_paveikslelis)
-    paveikslelis_array = np.expand_dims(paveikslelis_array, axis=0) #pridedamas bach dydis 1 (128.128.3)(1.128.128.3)
-
-    prognozes = modelis.predict(paveikslelis_array)
-    klase = np.argmax(prognozes)
-    tikslumas = float(np.max(prognozes))
-
-    return klases[klase], tikslumas
-
-def prognozuoti_su_svc(nuotraukos_kelias, klases, dydis=(128, 128)):
-    # Įkeliame modelį
-    modelis = joblib.load("issaugoti_modeliai/svc_modelis_rbf.pkl")
-
-    # Apdorojam paveikslėlį
-    img_bgr = cv2.imread(nuotraukos_kelias)
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-    img_resized = cv2.resize(img_rgb, dydis)
-
-    img_gray = rgb2gray(img_resized / 255.0)
-
-    pozymiai = hog(
-        img_gray,
-        pixels_per_cell=(16, 16),
-        cells_per_block=(3, 3),
-        orientations=9,
-        feature_vector=True
-    ).reshape(1, -1)
-
-    spejimas = modelis.predict(pozymiai)[0]
-    tikslumai = modelis.decision_function(pozymiai)
-    procentai = softmax(tikslumai)[0]  # paverčiam į tikimybes
-    tikslumas = float(np.max(procentai))
-
-    return klases[spejimas], tikslumas
-
-def prognozuoti_su_mobilenet(nuotraukos_kelias, klases, dydis=(224, 224)):
-    modelis = load_model("issaugoti_modeliai/mobilenet_modelis.h5") 
-
-    img = load_img(nuotraukos_kelias, target_size=dydis)
-    paveikslelis_array = img_to_array(img)
-    paveikslelis_array = np.expand_dims(paveikslelis_array, axis=0)
-
-    prognozes = modelis.predict(paveikslelis_array)
-    klase = np.argmax(prognozes)
-    tikslumas = float(np.max(prognozes))
-
-    return klases[klase], tikslumas
-
-def prognozuoti_su_cnn_hsv(nuotraukos_kelias, klases, dydis=(128, 128)):
-
-    modelis = load_model("issaugoti_modeliai/cnn_hsv_modelis.h5")
-
-    img_bgr = cv2.imread(nuotraukos_kelias)
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-    img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
-    img_resized = cv2.resize(img_hsv, dydis)
-
-    img_array = np.expand_dims(img_resized, axis=0)
-
-    prognozes = modelis.predict(img_array)
-    klase = np.argmax(prognozes)
-    tikslumas = float(np.max(prognozes))
-
-    return klases[klase], tikslumas
-
-def prognozuoti_su_importuotu_modeliu(nuotraukos_kelias, klases, dydis=(256, 256)):
-
-    modelis = load_model("issaugoti_modeliai/kagle_modelis.h5")
-
-    img = load_img(nuotraukos_kelias, target_size=dydis)
-    img_array = img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0  # normalizavimas, jei modelyje nėra Rescaling sluoksnio
-
-    prognozes = modelis.predict(img_array)
-    klase = np.argmax(prognozes)
-    tikslumas = float(np.max(prognozes))
-
-    return klases[klase], tikslumas
-
+from prognozavimas_vartotojui import ( prognozuoti_su_cnn,prognozuoti_su_svc,prognozuoti_su_mobilenet,prognozuoti_su_cnn_hsv,prognozuoti_su_atsiustu_modeliu)
 
 app = Flask(__name__)
 
@@ -169,7 +84,7 @@ def index():
                     'Tomato___Tomato_mosaic_virus',
                     'Tomato___healthy']
 
-                klase, tikslumas = prognozuoti_su_importuotu_modeliu(pilnas_kelias, klases_importuotas)
+                klase, tikslumas = prognozuoti_su_atsiustu_modeliu(pilnas_kelias, klases_importuotas)
                 rezultatas = f"Kito modelis: {klase} (tikslumas: {tikslumas*100:.2f}%)"
 
     return render_template('pagrindinis.html', rezultatas=rezultatas, paveikslelis=paveikslelio_kelias)
